aea8746a64e9cdc0c92d2e92a8a9f387b4f22c40
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2022-09-09 06:30:04 +0300

    Add files via upload

---
+++ HttpClientInterface.java
@@ -0,0 +1,6 @@
+package Sem1.FirstHomework;
+
+public interface HttpClientInterface {
+    public void get();
+    public void post();
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+package Sem1.FirstHomework;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.post();
+    }
+}
